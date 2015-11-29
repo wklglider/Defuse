6ea68715a9f6348cddc5c418af103e88107b8438
kuilin <wklglider@gmail.com>
2015-11-28 21:19:01 -0700

    n

---
+++ app/src/main/java/csci3320/defuse/GameOverActivity.java
@@ -0,0 +1,33 @@
+package csci3320.defuse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+public class GameOverActivity extends AppCompatActivity {
+
+    int status;
+
+    public GameOverActivity (int status) {
+        this.status = status;
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_finish);
+        if (status == 1) {
+            View background = findViewById(R.id.finishBackground);
+            background.setBackgroundColor(0xffffff);
+            ImageView imageView = (ImageView) findViewById(R.id.gameEndsImage);
+            imageView.setImageResource(R.drawable.bg_game_win);
+        } else {
+            View background = findViewById(R.id.finishBackground);
+            background.setBackgroundColor(0x000000);
+            ImageView imageView = (ImageView) findViewById(R.id.gameEndsImage);
+            imageView.setImageResource(R.drawable.bg_game_over);
+        }
+    }
+}
